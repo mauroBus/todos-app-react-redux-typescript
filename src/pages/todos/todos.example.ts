67ec7e9c3bf24f0043a8f280a0d5f7e2d553ed6f
Mauro Buselli <maurobuselli@gmail.com>
2020-02-15 21:23:37 -0300

    Create ToDos data list to development purpose

---
+++ src/pages/todos/todos.example.ts
@@ -0,0 +1,31 @@
+// List of todos for example and development purpose.
+export const TODOS: ToDo[] = [
+  {
+    completed: false,
+    id: 1,
+    starred: false,
+    text: 'Item 1',
+    createdAt: new Date('01/01/2020'),
+  },
+  {
+    completed: false,
+    id: 2,
+    starred: false,
+    text: 'Item 2',
+    createdAt: new Date('02/01/2020'),
+  },
+  {
+    completed: false,
+    id: 3,
+    starred: false,
+    text: 'Item 3',
+    createdAt: new Date('03/01/2020'),
+  },
+  {
+    completed: false,
+    id: 4,
+    starred: false,
+    text: 'Item 4',
+    createdAt: new Date('04/01/2020'),
+  },
+]
